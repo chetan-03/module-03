fe949df060a4e3aa7e7267dbdaffad482b03a9ce
chetan-03 <74251734+chetan-03@users.noreply.github.com>
2021-08-01 00:28:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,6 +22,9 @@
 
         NarrowItDownController.$inject = ['MenuSearchService', '$http', 'ApiBasePath']
         function NarrowItDownController(MenuSearchService, $http, ApiBasePath) {
+            function searchIsEmpty(search) {
+                return search.replace(/\s/g, "").length === 0;
+            };
             var menuSearch = this;
             menuSearch.searchTerm = "";
             menuSearch.found = [];
@@ -30,25 +33,24 @@
                 if (searchIsEmpty(menuSearch.searchTerm)) {
                     menuSearch.found = [];
                     menuSearch.valid = false;
+                    console.log(menuSearch.valid);
                     return;
+                } else {
+                    var searchForItems = MenuSearchService.getMatchedMenuItems(menuSearch.searchTerm);
+                    searchForItems
+                        .then((result) => {
+                            menuSearch.found = result;
+                            menuSearch.valid = (result.length) > 0;
+                            
+                        })
+                        .catch((error) => {
+                            console.log("Got error at MenuSearchService.getMatchedMenuItems in NarrowItDownController");
+                        });
                 }
-                var searchForItems = MenuSearchService.getMatchedMenuItems(menuSearch.searchTerm);
-                searchForItems
-                    .then((result) => {
-                        menuSearch.found = result;
-                        console.log(result);
-                        menuSearch.valid = (result.length) > 0;
-                    })
-                    .catch((error) => {
-                        console.log("Got error at MenuSearchService.getMatchedMenuItems in NarrowItDownController");
-                    });
             };
                 menuSearch.removeItem = (index) => {
                     menuSearch.found.splice(index, 1);
                 };
-                function searchIsEmpty(search) {
-                    return search.replace(/\s/g, "").lenght === 0;
-            };
         }
         MenuSearchService.$inject = ['$http', 'ApiBasePath']
         function MenuSearchService($http, ApiBasePath) {
